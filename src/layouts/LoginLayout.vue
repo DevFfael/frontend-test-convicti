72a541d1fbb1c333c516076c6d990dfc38fa16c3
Raffael Oliveira <raffaeloliveirap@gmail.com>
2023-09-01 18:23:10 -0300

    feature - create login page

---
+++ src/layouts/LoginLayout.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="loginContent">
+    <q-img src="../assets/logo.png" class="imageLogin" />
+    <q-form class="loginForm">
+      <div class="">
+        <label for="email">E-mail</label>
+        <q-input
+          v-model="email"
+          input-class="inputClassForm"
+          bg-color="white"
+          color="secondary"
+          type="email"
+          outlined
+        />
+      </div>
+      <div class="q-my-sm">
+        <label for="email">Senha</label>
+        <q-input
+          v-model="password"
+          input-class="inputClassForm"
+          bg-color="white"
+          color="secondary"
+          type="password"
+          outlined
+        />
+      </div>
+      <q-btn
+        padding="10px 0"
+        class="buttonForm"
+        color="accent"
+        label="Entrar"
+      />
+    </q-form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      email: null,
+      password: null,
+    };
+  },
+};
+</script>
+
+<style lang="sass">
+.loginContent
+  display: flex
+  align-items: center
+  justify-content: center
+  flex-direction: column
+  background-color: $bgPrimary
+  height: 100vh
+  color: $labelsSecondary
+
+.imageLogin
+  max-width: 400px
+  top: -100px
+
+.loginForm
+  width: 30%
+  font-size: 18px
+
+.inputClassForm
+  font-size: 18px
+
+.q-field__control
+  height:44px
+
+.buttonForm
+  width: 100%
+  margin-top: 12px
+</style>
